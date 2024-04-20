bc95904e0950b935d93a7b0ff64c47dc1d87d850
Treasure Ajefu <treasureajefu@gmail.com>
2024-04-20 23:36:30 +0100

    updated copy

--- src/assets/components/about.vue
+++ src/assets/components/about.vue
@@ -2,15 +2,15 @@
     <div>
         <main>
             <h1 class="text-2xl md:text-4xl w-full font-bold leading-tight">I'm a Frontend UI Engineer based in Lagos, Nigeria.</h1>
-            <p class="text-secondary text-2xl md:3xl my-6 font-medium">You're looking at the portfolio of a UI engineer with a passion for developing intuitive and visually appealing digital experiences. When I'm inot busy crafting clean and efficient code, you can find me either contributing to open-source projects or penning clear and concise tutorials that helps others navigate complex technical concepts (or maybe random opinionated articles, who knows?).</p>
+            <p class="text-secondary text-2xl md:3xl my-6 font-medium">You're looking at the portfolio of a UI engineer with a passion for developing intuitive and visually appealing digital experiences. I work with code <span class="text-white"> (React & Tailwind) </span> and no-code tools like <span class="text-white"> Framer </span>. When I'm not busy crafting clean and efficient code, you can find me either contributing to open-source projects or penning clear and concise tutorials that helps others navigate complex technical concepts (or maybe random opinionated articles, who knows?).</p>
             <p class="text-secondary text-2xl md:3xl my-6 font-medium">So if you're in need of a UI engineer who's freaking good at implementing designs pixel perfectly, look no further!</p>
 
-            <h1 class="text-2xl sm:text-3xl w-full font-bold leading-tight">Skills</h1>
-            <skills></skills>
+            <!-- <h1 class="text-2xl sm:text-3xl w-full font-bold leading-tight">Skills</h1>
+            <skills></skills> -->
             <section>
                 <h1 class="text-2xl sm:text-3xl w-full font-bold leading-tight">Now, you might be wondering... why do you need me on your team?</h1>
                     <ul class="ml-8">
-                        <li class="text-secondary text-2xl my-5 font-medium list-disc">Not to brag or anything :-), but I'm the person you would want to call if you wanted pixel-perfect translations of design to code. That's like... my specialty. So if that's why you're here, then hearty congratulations. You need not look further, you've got the right person for the job.</li>
+                        <li class="text-secondary text-2xl my-5 font-medium list-disc">Not to brag or anything :-), but I'm the person you would want to call if you wanted pixel-perfect translations of design to code or Framer. That's like... my specialty. So if that's why you're here, then hearty congratulations. You need not look further, you've got the right person for the job.</li>
                         <li class="text-secondary text-2xl my-5 font-medium list-disc">I make a great team player. Are you searching for someone with excellent communication skills, collaboration skills, problem solving and accountability? I've got those.</li>
                         <li class="text-secondary text-2xl my-5 font-medium list-disc">It's possible that I might (<strong>respectfully</strong>) debate with you over your own project, just so it turns out excellent. In advance, sorry (not sorry). Still open  to compromises either way.</li>
                         <li class="text-secondary text-2xl my-5 font-medium list-disc">If after this you're not quite sure about how I may be of use to you, just feel free to beep me. We can have a nice chit-chat and get to know each other. You can find some of my contact infos <router-link class="text-white underline decoration-wavy" to="contact">here</router-link>. Can't wait☺</li>
